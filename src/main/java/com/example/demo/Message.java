91df369f6afa50614fd942d81c7f6779ae6b6baf
tchekol1 <tchekol1@umbc.edu>
2019-08-23 11:02:40 -0400

    final

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+//MESSAGE
+//long id
+//String content
+//String posteddate (If you'd like, you can make this data type 'Date')
+//String sentby
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String content;
+    private String postdate;
+    private String sentby;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPostdate() {
+        return postdate;
+    }
+
+    public void setPostdate(String postdate) {
+        this.postdate = postdate;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+}
